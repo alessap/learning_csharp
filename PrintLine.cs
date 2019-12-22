0234f62d1d33c5bd3c3ff6f382df53caa53d19ab
Alessandro Pirrotta <alessandro.pirrotta@gmail.com>
2019-12-22 13:41:34 +0100

    move methods outside main program

---
+++ PrintLine.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace PrintLine{
+    class PrintLine{
+        public PrintLine() => Console.WriteLine("Something else");
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,35 +1,9 @@
 ﻿using System;
 
-namespace FibonacciExtension{
-    class Fibonacci {
-        // # constructor
-        public Fibonacci() {
-        }
-        public void GetFibonacciSeries(){
-            Console.WriteLine("Hello World! This is the beginning of Fibonacci's series");
-            string limit_str;
-            Console.Write("Enter the length of the series - ");
-            limit_str = Console.ReadLine();
-            Console.WriteLine("You entered '{0}'", limit_str);
-            int limit = Int32.Parse(limit_str);
-            int[] arr = new int[limit];
-            arr[0] = 0;
-            arr[1] = 1;
-            for (int i = 2; i < limit; i++){
-                arr[i] = arr[i-1] + arr[i-2] ;
-            }
-            for (int i = 0; i < limit; i++){
-                Console.WriteLine(arr[i] + " ");
-            }
-        }
-        // # desctructor
-        ~Fibonacci(){}
-    }
-}
-
 namespace cs_playground
 {
-    using FibonacciExtension;
+    using Fibonacci;
+    using PrintLine;
     class Program
     {
         static void Main(string[] args)
@@ -39,12 +13,11 @@ namespace cs_playground
             Console.Write("enter 2 if you want something else - ");
             choice = Console.ReadLine();
             if (choice == "1"){
-                Console.WriteLine("Fibonacci here");
                 Fibonacci fibo = new Fibonacci();
                 fibo.GetFibonacciSeries();
             }
             else if (choice != "1"){
-                Console.WriteLine("Something else");
+                PrintLine pl = new PrintLine();
             }
         }
     }
