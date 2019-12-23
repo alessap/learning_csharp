13dbda48651b19359a126a01bfdddfb29202c8b8
Alessandro Pirrotta <alessandro.pirrotta@gmail.com>
2019-12-23 13:16:11 +0100

    clean up

--- FibonacciExtension.cs
+++ FibonacciExtension.cs
@@ -7,11 +7,11 @@ namespace Fibonacci{
         }
         // method to print fibonacci series
         public void GetFibonacciSeries(){
-            Console.WriteLine("Hello World! This is the beginning of Fibonacci's series");
+            // Console.WriteLine("Hello World! This is the beginning of Fibonacci's series");
             string limit_str;
-            Console.Write("Enter the length of the series - ");
+            Console.Write("Enter the length of the series --> ");
             limit_str = Console.ReadLine();
-            Console.WriteLine("You entered '{0}'", limit_str);
+            // Console.WriteLine("You entered '{0}'", limit_str);
             int limit = Int32.Parse(limit_str);
             int[] arr = new int[limit];
             arr[0] = 0;
