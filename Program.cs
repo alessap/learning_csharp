21721dfc10b0ad5f91e00938ca19712cc0ae7a07
Alessandro Pirrotta <alessandro.pirrotta@gmail.com>
2019-12-23 13:29:16 +0100

    implement switch case

--- Program.cs
+++ Program.cs
@@ -10,26 +10,29 @@ namespace cs_playground
     {
         static void Main(string[] args)
         {
-            string choice;
+            string choice_str;
             Console.WriteLine("[0] Exit, [1] Fibonacci, [2] IsPrimeNumber, [3] WIP...");
             Console.Write("Enter your choice here --> ");
-            choice = Console.ReadLine();
-            if (choice == "1"){
-                Fibonacci fibo = new Fibonacci();
-                fibo.GetFibonacciSeries();
-                PrintLine pl = new PrintLine();
+            choice_str = Console.ReadLine();
+            int choice = Int32.Parse(choice_str);
+            PrintLine pl = new PrintLine();
+            switch (choice)
+            {
+                case 1:
+                    Fibonacci fibo = new Fibonacci();
+                    fibo.GetFibonacciSeries();
+                    break;
+                case 2:
+                    PrimeNumber pn = new PrimeNumber();
+                    Console.Write("Enter the number you want to check --> ");
+                    string n_str = Console.ReadLine();
+                    int n = Int32.Parse(n_str);
+                    pn.IsPrimeNumber(n);
+                    break;
+                default:
+                    break;
             }
-            else if (choice == "2"){
-                PrimeNumber pn = new PrimeNumber();
-                Console.Write("Enter the number you want to check --> ");
-                string n_str = Console.ReadLine();
-                int n = Int32.Parse(n_str);
-                pn.IsPrimeNumber(n);
-                PrintLine pl = new PrintLine();
-            }
-            else if (choice != "1"){
-                PrintLine pl = new PrintLine();
-            }
+            pl.PrintExitMessage();
         }
     }
 }
